d43575e37b40dfc6f56edd85b473d06720d018ab
root309 <choco00101x@gmail.com>
2024-02-12 04:41:45 +0900

    fix frontend ci

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/yukky.blog',
+  assetPrefix: '/yukky.blog/',
+}
 
-export default nextConfig;
+module.exports = nextConfig
